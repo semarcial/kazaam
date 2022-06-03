6509a46b70fd476d60eb9c4eb44008b1fdeb4b95
semarcial <sergio.marcial@shipt.com>
2022-06-03 18:22:57 -0500

    correcting tests

--- kazaam_test.go
+++ kazaam_test.go
@@ -38,7 +38,7 @@ func TestReregisterKazaamTransform(t *testing.T) {
 }
 
 func TestDefaultTransformsSetCardinarily(t *testing.T) {
-	if len(defaultSpecTypes) != 11 {
+	if len(defaultSpecTypes) != 12 {
 		t.Error("Unexpected number of default transforms. Missing tests?")
 	}
 }
